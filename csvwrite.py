c02cef4b807e5afe959a9c27e333bcbf5915f0b1
Priya Potti <59410993+Priya378@users.noreply.github.com>
2021-07-04 08:57:27 +0530

    Deleted experiment files

--- csvwrite.py
+++
@@ -1,9 +0,0 @@
-import csv
-import parser
-dict = {'name': 'krunal', 'age': 26, 'education': 'Engineering'}
-with open('data.csv', 'w') as f:
-    for key in dict.keys():
-        f.write("%s, %s\n" % (key, dict[key]))
-for i in parser.dic:
-    print(i)
-    print(parser.dic[i],"\n")
