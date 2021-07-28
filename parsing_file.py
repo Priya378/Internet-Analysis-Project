5fc8512135ef7a26d779c8e1f1c9e1cd9e7ca153
Priya Potti <59410993+Priya378@users.noreply.github.com>
2021-07-28 15:44:02 +0530

    Reduced redundancy

--- parsing_file.py
+++ parsing_file.py
@@ -2,31 +2,31 @@
 Parsing file to obtain a dictionary containing data of statistics.log file
 """
 import re
-filepath1 = 'mini.log'
-filepath2="statistics.log"
-with open(filepath2) as fp:
+FILEPATH1 = 'mini.log'
+FILEPATH2="statistics.log"
+with open(FILEPATH2) as fp:
     line=fp.readline()
     dic={}
     days=["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]
-    while(line):
-        if(line[0:3] in days):
+    while line:
+        if line[0:3] in days:
             key=line.strip()[:-9]
             dic[key]={}
-            line=fp.readline()  
+            line=fp.readline()
             while(line[0:3] not in days and line):
                 attr=line.split(":")[0].strip()
                 rhs=line.split(":")[1].strip()
-                if(attr=="Latency"):
-                    nums=re.findall("\d+.?\d*",rhs)
-                    if(nums):
+                if attr=="Latency":
+                    nums=re.findall(r"\d+.?\d*",rhs)
+                    if nums:
                         dic[key][attr]=nums[0]
                         dic[key]["Jitter"]=nums[1]
-                elif(attr=="Download" or attr=="Upload" or attr=="Packet Loss"):
-                    x=re.match("\d+.?\d*",rhs)
-                    if(x):
+                elif attr in ["Download","Upload","Packet Loss"]:
+                    x=re.match(r"\d+.?\d*",rhs)
+                    if x:
                         dic[key][attr]=x.group()
-                elif(attr=="Server"):
-                    x=re.match("[^\(]+",rhs)
-                    if(x):
+                elif attr=="Server":
+                    x=re.match(r"[^\(]+",rhs)
+                    if x:
                         dic[key][attr]=x.group().strip()
-                line=fp.readline()     
+                line=fp.readline()
