bff3a1b453a583a408323def76c2c14cf4e81e47
Priya Potti <59410993+Priya378@users.noreply.github.com>
2021-08-13 19:11:26 +0530

    Fixed white space errors

--- parsing_file.py
+++ parsing_file.py
@@ -3,31 +3,30 @@ Parsing file to obtain a dictionary containing data of statistics.log file
 """
 import re
 FILEPATH1 = 'mini.log'
-FILEPATH2="statistics.log"
+FILEPATH2 = "statistics.log"
 with open(FILEPATH2) as fp:
-    line=fp.readline()
-    dic={}
-    days=["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]
+    line = fp.readline()
+    dic = {}
+    days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
     while line:
         if line[0:3] in days:
-            key=line.strip()[:-9]
-            dic[key]={}
-            line=fp.readline()
+            key = line.strip()[:-9]
+            dic[key] = {}
+            line = fp.readline()
             while(line[0:3] not in days and line):
-                attr=line.split(":")[0].strip()
-                rhs=line.split(":")[1].strip()
-                if attr=="Latency":
-                    nums=re.findall(r"\d+.?\d*",rhs)
+                attr = line.split(":")[0].strip()
+                rhs = line.split(":")[1].strip()
+                if attr == "Latency":
+                    nums = re.findall(r"\d+.?\d*",rhs)
                     if nums:
-                        dic[key][attr]=nums[0]
-                        dic[key]["Jitter"]=nums[1]
-                elif attr in ["Download","Upload","Packet Loss"]:
-                    x=re.match(r"\d+.?\d*",rhs)
+                        dic[key][attr] = nums[0]
+                        dic[key]["Jitter"] = nums[1]
+                elif attr in ["Download", "Upload", "Packet Loss"]:
+                    x = re.match(r"\d+.?\d*",rhs)
                     if x:
-                        dic[key][attr]=x.group()
-                elif attr=="Server":
-                    x=re.match(r"[^\(]+",rhs)
+                        dic[key][attr] = x.group()
+                elif attr == "Server":
+                    x = re.match(r"[^\(]+",rhs)
                     if x:
-                        dic[key][attr]=x.group().strip()
-                line=fp.readline()
-                
+                        dic[key][attr] = x.group().strip()
+                line = fp.readline()
